909b183c5abbcff5a91d6a3e3072de554a59d41e
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-04-23 09:46:26 +0500

    add: training js #14 toString complete

--- JS Training/3-methods/14-number-method-toString.js
+++ JS Training/3-methods/14-number-method-toString.js
@@ -51,13 +51,40 @@ colorOf(1, 2 ,3) should return "#010203"
 That's all of your work. My work is print your color code on your screen.
 */
 
-function colorOf(r,g,b){
-  r = r.toString(16);
-  g = g.toString(16);
-  b = b.toString(16);
-  if (r.length < 2) {r = "0" + r};
-  if (g.length < 2) {g = "0" + g};
-  if (b.length < 2) {b = "0" + b};
- 
-    return "#"+r+g+b;
+// My extremely long and complicated solution -----------
+const colorOf = (r, g, b) => {
+    var num = [r, g, b];
+    var arr = [];
+    console.log(num);
+    for (let i = 0; i < num.length; i++) {
+        // if (num[i] >= 0 && num[i] <= 255) {
+        var strNum = num[i].toString(16);
+        arr.push(strNum)
+        // }
+        if (arr[i].length < 2) { arr[i] = `0${arr[i]}` }
+    }
+    return "#" + arr.join("");
 }
+document.write(colorOf(0, 111, 0));
+
+// given solution -----------------------------------
+// function colorOf(r,g,b){
+//   r = r.toString(16);
+//   g = g.toString(16);
+//   b = b.toString(16);
+//   if (r.length < 2) {r = "0" + r};
+//   if (g.length < 2) {g = "0" + g};
+//   if (b.length < 2) {b = "0" + b};
+
+//     return "#"+r+g+b;
+// }
+
+// codewars BEST solution ----------------------------
+// function colorOf(r, g, b) {
+//     return "#" + toHex(r) + toHex(g) + toHex(b)
+// }
+
+// function toHex(n) {
+//     var result = n.toString(16);
+//     return result.length == 1 ? "0" + result : result;
+// }
