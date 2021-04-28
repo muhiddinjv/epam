dd16733c901db6a5776cf93f02166b8fe4d5b7d0
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-04-28 11:06:33 +0500

    feat: add training js #18 split & join

--- JS Training/3-methods/18-string-method-concat-split-join.js
+++ JS Training/3-methods/18-string-method-concat-split-join.js
@@ -71,7 +71,11 @@ Ok, lesson is over. let's us do some task.
 Task
 Coding in function splitAndMerge, function accept 2 parameters:str and sp. str is a sentence. sp is a char as separator.
 
-First we need to divide the sentence into words(Use separator space); and then divide each word into characters(Use separator empty string); and then merge each characters with the specified sp; at last merge all the words(Use separator space) and return it.
+First we need to 
+1)divide the sentence into words(Use separator space); 
+2)divide each word into characters(Use separator empty string); 
+3)merge each characters with the specified sp; 
+4)merge all the words(Use separator space) and return it.
 
 for example:
 
@@ -81,6 +85,18 @@ splitAndMerge("Hello World!",".") should return "H.e.l.l.o W.o.r.l.d.!"
 splitAndMerge("Hello World!",",") should return "H,e,l,l,o W,o,r,l,d,!"
 */
 
-function splitAndMerge(str, sp) {
-  return str.split(" ").map(word => word.split("").join(sp)).join(" ");
+const splitAndMerge = (str, sp) => {
+  let arr = [];
+  let words = str.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    const el = words[i].split("").join(sp);
+    arr.push(el);
+  }
+  return arr.join(" ");
 }
+console.log(splitAndMerge("My name is John", "-"));
+
+// Codewars BEST solution
+// function splitAndMerge(str, sp) {
+//   return str.split(" ").map(word => word.split("").join(sp)).join(" ");
+// }
