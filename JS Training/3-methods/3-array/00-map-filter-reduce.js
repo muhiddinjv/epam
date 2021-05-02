8295d5322935549456735a3d72dd4b0365c01058
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-05-02 18:32:06 +0500

    init: practice map-filter-reduce methods

--- JS Training/3-methods/3-array/00-map-filter-reduce.js
+++ JS Training/3-methods/3-array/00-map-filter-reduce.js
@@ -1,12 +1,15 @@
 const arr = [1, 2, 3, 4, 5, 6, 7];
 
-// return odd numbers thru filter
+// return ODD numbers thru filter
 const newArr1 = arr.filter(a => a % 2 === 1);
 
-// return even numbers thru filter
+// return EVEN numbers thru filter
 const newArr2 = arr.filter(a => a % 2 === 0);
 
 // map's purpose is to turn each element from the original array into a new element & make a new array. It uses 1 callback "a".
-const newArr3 = arr.map(a => a * 10);
+const newArr3 = arr.map(a => a * 3);
+const newArr4 = arr.map(a => a * a);
 
-console.log(newArr3);
+// console.log(newArr2);
+// console.log(newArr1);
+console.log(newArr4);
