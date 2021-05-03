6dc5c975ab2151d9ef59a210cb863c8892cb3ce3
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-05-03 08:36:22 +0500

    init: es6 class & inheritance

---
+++ CoreJS/classinheritance/javascriptES6.js
@@ -0,0 +1,37 @@
+"use strict"
+
+class User {
+  constructor(username, email, password) {
+    this.username = username;
+    this.email = email;
+    this.password = password;
+  }
+  static countUsers() {
+    console.log('There are 50 users');
+  }
+
+  register() {
+    console.log(this.username + " is now registered");
+  }
+}
+// let bob = new User("Bob", "bob@email.com", "1234")
+// bob.register();
+// User.countUsers();
+
+//INHERIT & EXTEND CLASSES
+class Member extends User {
+  constructor(username, email, password, memberPackage) {
+    super(username, email, password);
+    this.package = memberPackage;
+  }
+
+  getPackage() {
+    console.log(`${this.username} is subscribed to ${this.package} package and his email is ${this.email} and his password is not ${this.password}`);
+  }
+}
+
+let mike = new Member("Mike", "mike@mike.com", "12355", "Platinum");
+
+
+mike.getPackage();
+mike.register();
