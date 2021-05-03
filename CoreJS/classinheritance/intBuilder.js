555f1c1d0e5604005ea3a77bd6633cf092e159ec
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-05-04 00:40:48 +0500

    init: start intBuilder task

---
+++ CoreJS/classinheritance/intBuilder.js
@@ -0,0 +1,11 @@
+class IntBuilder {
+  constructor(int) {
+    this.int = int;
+  }
+  plus(...n) { return n };
+  minus(...n) { return n };
+  multiply(n) { return n };
+  divide(n) { return n };
+  mod(n) { return n };
+  get(int) { return int };
+}
