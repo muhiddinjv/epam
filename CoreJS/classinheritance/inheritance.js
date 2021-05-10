94dce1a481008e8555631d022fe259887bde51bb
iMbassador <50528874+iMbassador@users.noreply.github.com>
2021-05-11 00:06:44 +0500

    feat: add filter to sort out chosen letter

--- CoreJS/classinheritance/inheritance.js
+++ CoreJS/classinheritance/inheritance.js
@@ -9,9 +9,23 @@ class Base {
     return this;
   };
   minus(n) { return this.val.slice(0, -n) }; // STRING
-  multiply(n) { return strBuilder.val.repeat(n) }
+  divide(n) { return this.val.slice(0, n) }
+  allChosenChars(str) {
+    let char = this.val.split('');
+    const chars = char.filter(a => a === str);
+    return chars
+  }
+  remove(str) {
+    let char = this.val.split('');
+    let chars = char.filter(val => val === str ? val.indexOf() : 0);
+    return chars;
+    // let index = this.x.indexOf('s');
+    // this.x =
+    //   this.x.substring(0, index) +
+    //   this.x.substring(index + str.length, this.x.length);
+    // return this;
+  }
 }
-
 class IntBuilder extends Base {
   constructor(val) {
     val = typeof val === 'number' ? val : 0;
@@ -47,22 +61,20 @@ StringBuilder.prototype = Object.create(Base.prototype);
 
 // NUMBER ------------------------------------------------------
 let intBuilder = new IntBuilder(10);
-console.log(intBuilder.plus(2, 3, 2).minus(1, 2).multiply(2).divide(4).mod(3).value);
+console.log(intBuilder
+  .plus(2, 3, 2)
+  .minus(1, 2)
+  .multiply(2)
+  .divide(4)
+  .mod(3)
+  .value);
 console.log(intBuilder.stored); // get method
 console.log(IntBuilder.random(10, 100)); // static method
 
 // STRING -------------------------------------------------------
 let strBuilder = new StringBuilder("Hello");
 console.log(strBuilder.plus(' all', '!').value);
-// console.log(strBuilder.minus(4));
-// console.log(StringBuilder.multiply(2));
-// console.log(StringBuilder.minus());
-
-function check(val) {
-  if (val = typeof val === "string") {
-    console.log('string');
-  } else {
-    console.log('number');
-  }
-}
-check(1)
+console.log(strBuilder.minus(4));
+console.log(StringBuilder.multiply(2));
+console.log(strBuilder.divide(4));
+console.log(strBuilder.remove("l"));
