ab73d6c8a30cb7a91e0b0707698392f85081cc72
Emuallim <muhiddin001@gmail.com>
2021-07-20 10:46:12 +0500

    new update jul20

--- js-training/core-js-101/01-strings-tasks.js
+++ js-training/core-js-101/01-strings-tasks.js
@@ -5,15 +5,8 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Returns the result of concatenation of two strings.
- *
- * @param {string} value1
- * @param {string} value2
- * @return {string}
- *
- * @example
  *   'aa', 'bb' => 'aabb'
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
@@ -25,11 +18,6 @@ function concatenateStrings(value1, value2) {
 
 /**
  * Returns the length of given string.
- *
- * @param {string} value
- * @return {number}
- *
- * @example
  *   'aaaaa' => 5
  *   'b'     => 1
  *   ''      => 0
@@ -42,12 +30,7 @@ function getStringLength(value) {
  * Returns the result of string template and given parameters firstName and lastName.
  * Please do not use concatenation, use template string :
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
- *
- * @param {string} firstName
- * @param {string} lastName
- * @return {string}
- *
- * @example
+ * 
  *   'John','Doe'      => 'Hello, John Doe!'
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
@@ -57,11 +40,7 @@ function getStringFromTemplate(firstName, lastName) {
 
 /**
  * Extracts a name from template string 'Hello, First_Name Last_Name!'.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
+ * 
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
@@ -73,11 +52,7 @@ function extractNameFromTemplate(value) {
 
 /**
  * Returns a first char of the given string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
+ * 
  *   'John Doe'  => 'J'
  *   'cat'       => 'c'
  */
@@ -87,11 +62,7 @@ function getFirstChar(value) {
 
 /**
  * Removes a leading and trailing whitespace characters from string.
- *
- * @param {string} value
- * @return {string}
- *
- * @example
+ * 
  *   '  Abracadabra'    => 'Abracadabra'
  *   'cat'              => 'cat'
  *   '\tHello, World! ' => 'Hello, World!'
@@ -102,12 +73,7 @@ function removeLeadingAndTrailingWhitespaces(value) {
 
 /**
  * Returns a string that repeated the specified number of times.
- *
- * @param {string} value
- * @param {string} count
- * @return {string}
- *
- * @example
+ * 
  *   'A', 5  => 'AAAAA'
  *   'cat', 3 => 'catcatcat'
  */
@@ -117,12 +83,7 @@ function repeatString(value, count) {
 
 /**
  * Remove the first occurrence of string inside another string
- *
- * @param {string} str
- * @param {string} value
- * @return {string}
- *
- * @example
+ * 
  *   'To be or not to be', 'not'  => 'To be or  to be'
  *   'I like legends', 'end' => 'I like legs',
  *   'ABABAB','BA' => 'ABAB'
@@ -133,11 +94,7 @@ function removeFirstOccurrences(str, value) {
 
 /**
  * Remove the first and last angle brackets from tag string
- *
- * @param {string} str
- * @return {string}
- *
- * @example
+ * 
  *   '<div>' => 'div'
  *   '<span>' => 'span'
  *   '<a>' => 'a'
@@ -150,10 +107,6 @@ function unbracketTag(str) {
 /**
  * Converts all characters of the specified string into the upper case
  *
- * @param {string} str
- * @return {string}
- *
- * @example
  *   'Thunderstruck' => 'THUNDERSTRUCK'
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
@@ -164,10 +117,6 @@ function convertToUpperCase(str) {
 /**
  * Extracts e-mails from single string with e-mails list delimeted by semicolons
  *
- * @param {string} str
- * @return {array}
- *
- * @example
  *   'angus.young@gmail.com;brian.johnson@hotmail.com;bon.scott@yahoo.com'
  *   => [
  *      'angus.young@gmail.com',
@@ -184,12 +133,6 @@ function extractEmails(str) {
  * Returns the string representation of rectangle with specified width and height
  * using pseudograhic chars
  *
- * @param {number} width
- * @param {number} height
- * @return {string}
- *
- * @example
- *
  *            '┌────┐\n'+
  *  (6,4) =>  '│    │\n'+
  *            '│    │\n'+
@@ -203,19 +146,49 @@ function extractEmails(str) {
  *             '└──────────┘\n'
  *
  */
-function getRectangleString(/* width, height */) {
-  throw new Error('Not implemented');
-}
+//  function getRectangleString(width, height) {
+//   for (let i = 0; i < height; i++) {
+//     let row = '';
+//     for (let j = 0; j < width; j++) {
+//       row += '*';
+//     }
+//     return row;
+//   }
+// }
+// document.write(getRectangleString(8,8))
+
+// for(int i = 0; i < height; i++){
+//   for(int j = 0; i < width; j++){
+//       System.out.print("*");
+//   }
+//   System.out.print("\n");
+// }
+
+// var num=parseInt(prompt('Please enter the number of rows of the rectangle (must be a number greater than 0)'));
+// for(var i=1;i<=num;i++){
+//     for (var j = 1; j <= 2; j++) {
+//         document.write("<span></span>");
+//     }
+    
+//     for (var j = 1; j <=num; j++) {
+//         if(j==1 || j==num){
+//             document.write("⭐");
+//         }else if(i==1){
+//             document.write("⭐"); 
+//         }else if(i==num){
+//             document.write("⭐"); 
+//         }else{
+//             document.write("<span></span>");
+//         }    
+//     }
+//     document.write("<br>");
+// }
+
 
 
 /**
  * Encode specified string with ROT13 cipher
  * See details:  https://en.wikipedia.org/wiki/ROT13
- *
- * @param {string} str
- * @return {string}
- *
- * @example
  *
  *   'hello' => 'uryyb'
  *   'Why did the chicken cross the road?' => 'Jul qvq gur puvpxra pebff gur ebnq?'
@@ -224,16 +197,23 @@ function getRectangleString(/* width, height */) {
  *    => 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm'
  *
  */
-function encodeToRot13(/* str */) {
-  throw new Error('Not implemented');
+ const encodeToRot13 = str => {
+  const input = "abcdefghijklmnopqrstuvwxyz".split('');
+  const msg = str.split('');
+
+  console.log(input.length);
+  console.log(msg.length);
+  
+    
+      for(let i = 0; i < input.length; i++){
+        if (msg.length > input.length){
+          console.log('i is higher than input length')
+        }
+      }
 }
 
 /**
  * Returns true if the value is string; otherwise false.
- * @param {string} value
- * @return {boolean}
- *
- * @example
  *   isString() => false
  *   isString(null) => false
  *   isString([]) => false
@@ -259,10 +239,6 @@ function isString(/* value */) {
  * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
  * Function returns the zero-based index of specified card in the initial deck above.
  *
- * @param {string} value
- * @return {number}
- *
- * @example
  *   'A♣' => 0
  *   '2♣' => 1
  *   '3♣' => 2
@@ -292,3 +268,5 @@ function getCardId(/* value */) {
 //   isString,
 //   getCardId,
 // };
+
+encodeToRot13('hadfasfdasfdadsfasdfasddfasfdsafsafdasdfsadffasdfasdfsdafi');
