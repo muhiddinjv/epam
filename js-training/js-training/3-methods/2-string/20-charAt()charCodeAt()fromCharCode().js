26dfe04c0e404848a267d292a85d632ce79379e7
Emuallim <muhiddin001@gmail.com>
2021-07-21 10:19:38 +0500

    try & fail (from)char(Code)At

--- js-training/js-training/3-methods/2-string/20-charAt()charCodeAt()fromCharCode().js
+++ js-training/js-training/3-methods/2-string/20-charAt()charCodeAt()fromCharCode().js
@@ -1,39 +1,39 @@
 /*
 7 kyu: Training JS #20: Methods of String object--charAt() charCodeAt() and fromCharCode()
 
-This time we learn three string methods used to single character operation :charAt() charCodeAt() and fromCharCode(). Their usage:
+This time we learn three String methods used to single character operation :charAt() charCodeAt() and fromCharCode(). Their usage:
 
-stringObject.charAt(index)
-stringObject.charCodeAt(index)
+StringObject.charAt(index)
+StringObject.charCodeAt(index)
 String.fromCharCode(num1,num2,...,numX)
 charAt() method can return to the specified location of the character. like this:
 
-var str="abcde";
-console.log(str.charAt(0));
-console.log(str.charAt(1));
+var file="abcde";
+console.log(file.charAt(0));
+console.log(file.charAt(1));
 //output:
 a
 b
-In practical use, the charAt() method is usually replaced by str[index]. Because it is more convenient:
+In practical use, the charAt() method is usually replaced by file[index]. Because it is more convenient:
 
-var str="abcde";
-console.log(str[0]);
-console.log(str[1]);
+var file="abcde";
+console.log(file[0]);
+console.log(file[1]);
 //output:
 a
 b
 
 charCodeAt() method can return the specified location of the character of the Unicode encoding. This return value is an integer between 0 and 65535.
 
-Method charCodeAt() and charAt() operation is similar, but the former is returned to the specified location of the character encoding, and the latter is returned to the character string.
+Method charCodeAt() and charAt() operation is similar, but the former is returned to the specified location of the character encoding, and the latter is returned to the character String.
 
-Unicode is a computing industry standard for the consistent encoding, representation, and handling of text expressed in most of the world's writing systems.This is the reference information of unicode.
+Unicode is a computing indufiley standard for the consistent encoding, representation, and handling of text expressed in most of the world's writing systems.This is the reference information of unicode.
 
-This example will display the character value and unicode value of each characters in str:
+This example will display the character value and unicode value of each characters in file:
 
-var str="abcde";
-for (var i=0;i<str.length;i++){
-  console.log(str.charAt(i),str.charCodeAt(i));
+var file="abcde";
+for (var i=0;i<file.length;i++){
+  console.log(file.charAt(i),file.charCodeAt(i));
 }
 
 //output:
@@ -42,7 +42,7 @@ b 98
 c 99
 d 100
 e 101
-If parameter index of charCodeAt() is omitted, its returned value will be the first character of stringObject. So the parameter can be omitted when the string length is 1; in other cases, you can't omit the parameter, unless you want to get the first character. An example of omitted parameter:
+If parameter index of charCodeAt() is omitted, its returned value will be the first character of StringObject. So the parameter can be omitted when the String length is 1; in other cases, you can't omit the parameter, unless you want to get the first character. An example of omitted parameter:
 
 var chars="abcde".split("");
 for (var i=0;i<chars.length;i++){
@@ -57,27 +57,26 @@ a for apple
 99
 100
 101
-fromCharCode() can accept one or more specified Unicode values, and then return a string. The method is a static method of String. It cannot be used as a method of the String object you have created. So its syntax should be String.fromCharCode() instead of stringObject.fromCharCode(). See example:
+fromCharCode() can accept one or more specified Unicode values, and then return a String. The method is a static method of String. It cannot be used as a method of the String object you have created. So its syntax should be String.fromCharCode() instead of StringObject.fromCharCode(). See example:
 
 var chars=[];
 for (var i=97;i<123;i++){
   chars.push(String.fromCharCode(i));
 }
 console.log(chars.join(""))
-//output:
-abcdefghijklmnopqrstuvwxyz
-This code uses fromCharCode() to create a string from a to z.
+//output: abcdefghijklmnopqrstuvwxyz
+This code uses fromCharCode() to create a String from a to z.
 
 When fromCharCode() has only one parameter, it is very much like the reverse operation of charCodeAt(). they are a pair of good partners. We often see them in the kata about the shift encryption. See an example:
 
-function decode(str){
-  var chars=str.split("");
+function decode(file){
+  var chars=file.split("");
   for (var i=0;i<chars.length;i++)
     chars[i]=String.fromCharCode(chars[i].charCodeAt()-1);
   return chars.join("");
 }
-function encode(str){
-  var chars=str.split("");
+function encode(file){
+  var chars=file.split("");
   for (var i=0;i<chars.length;i++)
     chars[i]=String.fromCharCode(chars[i].charCodeAt()+1);
   return chars.join("");
@@ -95,7 +94,7 @@ This is a simple example of the shift of encryption, encryption to the right (Un
 Ok, lesson is over. let's us do some task.
 
 Task
-Coding in function topSecret, function accept 1 parameter:file. file is an encrypted string.
+Coding in function topSecret, function accept 1 parameter:file. file is an encrypted String.
 
 Encryption using the right shift 3. Your job is to decrypt the file and read the contents of the file, and then answer my three questions. If the answer is correct, you will pass the test. Note: the decryption should be left shift; Shift is only for uppercase and lowercase letters, other characters will remain the same; The shift mode is a cyclic shift, for example:
 
@@ -131,31 +130,25 @@ You only need to choose one of the text as the answer to the question. That is t
 */
 
 const topSecret = file => {
-  return file;
+  
+  function decode(file) {
+  var chars = file.split("");
+  for (var i = 0; i < chars.length; i++)
+    chars[i] = String.fromCharCode(chars[i].charCodeAt() - 1);
+  return chars.join("");
+}
+function encode(file) {
+  var chars = file.split("");
+  for (var i = 0; i < chars.length; i++)
+    chars[i] = String.fromCharCode(chars[i].charCodeAt() + 1);
+  return chars.join("");
+}
+var s = "abcde"
+s = encode(s);
+console.log("encode:", s)    //encryption s
+s = decode(s);
+console.log("decode:", s)    //decryption s
 }
 
 console.log(topSecret("hi"));
 
-// function decode(str) {
-//   var chars = str.split("");
-//   for (var i = 0; i < chars.length; i++)
-//     chars[i] = String.fromCharCode(chars[i].charCodeAt() - 1);
-//   return chars.join("");
-// }
-// function encode(str) {
-//   var chars = str.split("");
-//   for (var i = 0; i < chars.length; i++)
-//     chars[i] = String.fromCharCode(chars[i].charCodeAt() + 1);
-//   return chars.join("");
-// }
-// var s = "abcde"
-// s = encode(s);
-// console.log("encode:", s)    //encryption s
-// s = decode(s);
-// console.log("decode:", s)    //decryption s
-
-// var chars = [];
-// for (var i = 97; i < 123; i++) {
-//   chars.push(String.fromCharCode(i));
-// }
-// console.log(chars.join(""))
