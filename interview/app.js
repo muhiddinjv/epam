711338c120aa1cd2f86a4dfb28acf3587366c27b
Emuallim <muhiddin001@gmail.com>
2021-07-22 07:35:01 +0500

    feat: add background color

--- interview/app.js
+++ interview/app.js
@@ -3,18 +3,20 @@
 // COMPONENT 1 ------------------------------
 const ToDoList = () => {
   return (
-    <div>
-      <h2>After EPAM Interview</h2>
-      <h3>Suggestions for a candidate</h3>
-      <ol>
-        <li>JS Core: Event Loop, Queue, Stack; Promises; Generator</li>
-        <li>Data Types & Structures: Set, Symbol, Map</li>
-        <li>Git: branches, merge, squash/rebase</li>
-        <li>Solve more algorithmic tasks</li>
-        <li>SHIFT+ALT+F to format code</li>
-        <li>OOP: SOLID principles</li>
-        <li>CI/CD</li>
-      </ol>
+    <div className="main">
+      <div>
+        <h2>After EPAM Interview</h2>
+        <h3>Suggestions for a candidate</h3>
+        <ol>
+          <li>JS Core: Event Loop, Queue, Stack; Promises; Generator</li>
+          <li>Data Types & Structures: Set, Symbol, Map</li>
+          <li>Git: branches, merge, squash/rebase</li>
+          <li>Solve more algorithmic tasks</li>
+          <li>SHIFT+ALT+F to format code</li>
+          <li>OOP: SOLID principles</li>
+          <li>CI/CD</li>
+        </ol>
+      </div>
     </div>
   );
 };
@@ -32,34 +34,63 @@ const Async = () => {
     setTimeout(show, 3000); // Async function here  
     const show = () => document.getElementById("demo").innerHTML = "I Love You!";
   `;
+
+  const promise = `
+    let myPromise = new Promise(function(resolve, reject) {
+      // "Producing Code" (May take some time)
+      
+        resolve(); // when successful
+        reject();  // when error
+      });
+      
+      // "Consuming Code" (Must wait for a fulfilled Promise)
+      myPromise.then(
+        function(value) { /* code if successful */ },
+        function(error) { /* code if some error */ }
+      );
+  `;
   const asyncFunc = () => {
-    setTimeout(show, 3000); // Async function here  
-    const show = () => document.write("I Love You!");
-  }
+    setTimeout(() => {
+      alert("I love you baby!");
+    }, 3000); // Async function here
+  };
   return (
-    <div>
-      <h3>Asyncronous JavaScript (Event Loop)</h3>
-      <div className="callbacks">
+    <div className="main">
+      <div>
+        <h3>Asyncronous JS (Event Loop)</h3>
+        <div className="callbacks">
+          {/* CALLBACKS ----------------------------------------- */}
+          <h4>JS Callbacks</h4>
+          <p className="width_600">
+            A callback is a function passed as an argument to another function.
+            It can run after another function has finished. Where callbacks
+            really shine are in async functions.
+          </p>
+          <pre className="code width_600">{callBackCode}</pre>
+        </div>
 
-      {/* JAVASCRIPT CALLBACKS */}
-        <h4>JS Callbacks</h4>
-        <p className="width_600">
-          A callback is a function passed as an argument to another function. It
-          can run after another function has finished. Where callbacks really
-          shine are in async functions, where one function has to wait for
-          another function (like waiting for a file to load)
-        </p>
-        <pre className="code width_600">{callBackCode}</pre>
-      </div>
+        {/* ASYNCHRONOUS ----------------------------------------- */}
+        <div className="async">
+          <h4>JS Asyncronous</h4>
+          <p className="width_600">
+            Functions (show) running in parallel with other functions
+            (setTimeout()) are called asynchronous = one function has to wait
+            for another function (like waiting for a file to load, timeout or
+            interval) <i onClick={asyncFunc}>Click here and wait 3 seconds!</i>
+          </p>
+          <pre className="code width_600">{asynCode}</pre>
+        </div>
 
-      {/* ASYNCHRONOUS JAVASCRIPT */}
-      <div className="async">
-        <h4>JS Asyncronous</h4>
-        <p className="width_600" onClick={asyncFunc}>
-          Functions (calculate) running in parallel with other functions
-          (setTimeout()) are called asynchronous. Click here to see the result!
-        </p>
-        <pre className="code width_600">{asynCode}</pre>
+        {/* PROMISES ----------------------------------------- */}
+        <div className="promise">
+          <h4>JS Promises</h4>
+          <p className="width_600">
+            "Producing code" can take some time. "Consuming code" must wait for
+            the result. A Promise is an object that links producing code and
+            consuming code.
+          </p>
+          <pre className="code width_600">{promise}</pre>
+        </div>
       </div>
     </div>
   );
@@ -91,13 +122,15 @@ class App extends React.Component {
   render() {
     const man = { name: "Bill", height: 145 };
     return (
-      <div>
+      <div className="main">
+        <div>
         <ToDoList />
         <h5 onClick={this.changeColor}>
           {this.state.action} this {this.state.color} {this.state.brand} owned
           by {this.state.owners[0]}!
         </h5>
         <Async />
+      </div>
       </div>
     );
   }
