25a7d97b61e7add44ab12f56602c3266d7978a24
Emuallim <muhiddin001@gmail.com>
2021-07-24 10:29:41 +0500

    fix: make accordion responsive

--- interview/app.js
+++ interview/app.js
@@ -70,10 +70,16 @@ class Async extends React.Component {
               <li>Rejected</li>
             </ol>
             <p>
-              The Promise object supports two properties: <b>state</b> and <b>result</b>.
-              <br/>While a Promise object is 'pending' (working), the result is undefined. 
-              <br/>When a Promise object is 'fulfilled', the result is a value. 
-              <br/>When a Promise object is 'rejected', the result is an error object."
+              The Promise object supports two properties: <b>state</b> and{" "}
+              <b>result</b>.
+              <br />
+              While a Promise object is 'pending' (working), the result is
+              undefined.
+              <br />
+              When a Promise object is 'fulfilled', the result is a value.
+              <br />
+              When a Promise object is 'rejected', the result is an error
+              object."
             </p>
           </div>
         ),
@@ -99,6 +105,34 @@ class Async extends React.Component {
             </tbody>
           </table>
         ),
+      },{
+        title: "Promise HowTo",
+        text: "Promise.then() takes two (optional) arguments, a callback for success and another for failure. You can add a callback forsuccess or failure only",
+        code: `
+        const show = sum => document.getElementById("demo").innerHTML = sum;
+
+        let myPromise = new Promise((resolve, reject) => {
+          let x = 0; // some code (try to change x to 5)
+          x == 0 ? resolve("OK") : reject("Error");
+        });
+
+        myPromise.then(
+          (value) => {show(value)},
+          (error) => {show(error)}
+        );`,
+      },{
+        title: "Async (A)wait",
+        text: (<p>Async and await make promises easier to write! 
+        <b> 1-Async</b> makes a function return a Promise.  
+        <b> 2-Await</b> makes a function wait for a Promise.</p>),
+        code: `
+        async function show() {
+          let myPromise = new Promise((resolve, reject) => {
+            setTimeout(() => { resolve("I love You !!"); }, 3000);
+          });
+          document.getElementById("demo").innerHTML = await myPromise;
+        }
+        show();`,
       },
     ];
   }
@@ -106,7 +140,7 @@ class Async extends React.Component {
   render() {
     return (
       <div className="main">
-        <div className="width_600">
+        <div className="width70pr">
           <h2
             onClick={() =>
               setTimeout(() => alert("This is asynchronous js baby!"), 3000)
@@ -114,65 +148,47 @@ class Async extends React.Component {
           >
             Core JS (click & wait 3s)
           </h2>
-          {/* CALLBACKS ----------------------------------------- */}
+          {/* CALLBACKS --------------------------------------------- */}
           <Accordion
             title={this.state[0].title}
             text={this.state[0].text}
             code={this.state[0].code}
           />
 
-          {/* ASYNCHRONOUS ----------------------------------------- */}
+          {/* ASYNCHRONOUS ------------------------------------------ */}
           <Accordion
             title={this.state[1].title}
             text={this.state[1].text}
             code={this.state[1].code}
           />
 
-          {/* PROMISES ----------------------------------------- */}
+          {/* PROMISES ---------------------------------------------- */}
           <Accordion
             title={this.state[2].title}
             text={this.state[2].text}
             code={this.state[2].code}
           />
 
+          {/* PROMISES-OBJ-PROPS ------------------------------------ */}
           <Accordion
             title={this.state[3].title}
             text={this.state[3].text}
             table={this.state[3].table}
           />
 
-          <div className="promise">
-            <div className="note">
-              <p>
-                You cannot access the Promise properties <b>state</b> and{" "}
-                <b>result</b>.
-                <br /> You must use a Promise method to handle promises.
-              </p>
-            </div>
-            <div>
-              <h4>How to Use a Promise</h4>
-              {/* <pre>{this.state.promiseHowTo}</pre> */}
-              <div className="note">
-                <p>
-                  Promise.then() takes two (optional) arguments, a callback for
-                  success and another for failure. You can add a callback for
-                  success or failure only.
-                </p>
-                {/* <pre>{this.state.callbackVsPromise}</pre> */}
-              </div>
-            </div>
-            <div>
-              <h4>JS Async</h4>
-              <div className="note">
-                <p>
-                  Async and await make promises easier to write! <br /> async
-                  makes a function return a Promise <br /> await makes a
-                  function wait for a Promise
-                </p>
-              </div>
-              {/* <pre>{this.state.asyncAwait}</pre> */}
-            </div>
-          </div>
+          {/* PROMISES-HowTo ------------------------------------ */}
+          <Accordion
+            title={this.state[4].title}
+            text={this.state[4].text}
+            code={this.state[4].code}
+          />
+
+          {/* PROMISES-HowTo ------------------------------------ */}
+          <Accordion
+            title={this.state[5].title}
+            text={this.state[5].text}
+            code={this.state[5].code}
+          />        
         </div>
       </div>
     );
@@ -181,7 +197,6 @@ class Async extends React.Component {
 
 // ACCORDION ----------------------------------
 class Accordion extends React.Component {
-  // const [show, toggleShow] = useState(false);
   constructor(props) {
     super(props);
     this.state = { show: false };
@@ -224,7 +239,7 @@ class App extends React.Component {
     };
   }
 
-  changeColor = () => {
+  changeValue = () => {
     this.setState({
       action: "Dont click",
       color: "blue",
@@ -237,10 +252,10 @@ class App extends React.Component {
   render() {
     // const man = { name: "Bill", height: 145 };
     return (
-      <div className="main">
+      <div>
         <div>
           <ToDoList />
-          <h5 style={{ color: this.state.color }} onClick={this.changeColor}>
+          <h5 style={{ color: this.state.color }} onClick={this.changeValue}>
             {this.state.action} this {this.state.color} {this.state.brand} owned
             by {this.state.owners[0]}!
           </h5>
