e3dd4efeffa504eb371abb98b03bff0b65623f0e
Emuallim <muhiddin001@gmail.com>
2021-08-04 19:51:41 +0500

    refactor: remove unnecessary spaces

--- internship/autocomplete/app.js
+++ internship/autocomplete/app.js
@@ -1,27 +1,21 @@
 const search = document.querySelector("#search");
 const matchList = document.querySelector("#match-list");
-
 // THE LOGICS -------------------------------------------
 // Search states.json and filter it
 const searchStates = async (searchText) => {
   const res = await fetch("states.json");
   const states = await res.json();
-
-  //Get matches to current text input
+  // console.log(states)
+  //Get matches to current text input--------------------
   let matches = states.filter((state) => {
     const regex = new RegExp(`^${searchText}`, "gi");
     return state.name.match(regex) || state.abbr.match(regex);
   });
 
-  // if text is deleted, then clear out results
-  if (searchText.length === 0){ 
-    matches = []; 
-    matchList.innerHTML="";
-  };
+  // if text is deleted, clear out results----------
+  if (searchText.length === 0){matches = []; matchList.innerHTML="";};
   outputHtml(matches);
 };
-
-
 // THE OUTPUT -------------------------------------------
 // Show results in HTML
 const outputHtml = matches => {
@@ -34,9 +28,5 @@ const outputHtml = matches => {
         </h4>
         <small>Lat: ${match.lat} / long: ${match.long} </small>
       </div>
-    `).join('');
-    matchList.innerHTML=html;
-  }
-};
-
+    `).join(''); matchList.innerHTML=html;}};
 search.addEventListener("input", () => searchStates(search.value));
---
+++ internship/autocomplete/play.js
@@ -0,0 +1,8 @@
+let matchList = document.querySelector('#match-list');
+let search = document.querySelector('#search');
+
+// THE LOGICS -------------------------------------------
+// Search states.json and filter it
+const searchStates = async (searchText) => {
+    console.log(searchText);
+}
