996540e3815b98a1498439b854d88bcf60e1af0b
Emuallim <muhiddin001@gmail.com>
2021-08-10 16:14:49 +0500

    feat: add task 2

--- internship/autocomplete/play.js
+++
@@ -1,8 +0,0 @@
-let matchList = document.querySelector('#match-list');
-let search = document.querySelector('#search');
-
-// THE LOGICS -------------------------------------------
-// Search states.json and filter it
-const searchStates = async (searchText) => {
-    console.log(searchText);
-}
