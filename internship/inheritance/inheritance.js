37897d32910da039bc1ca46ee637a459933337f0
Emuallim <muhiddin001@gmail.com>
2021-08-23 23:28:50 +0500

    feat: add inheritance task solution

--- internship/inheritance/inheritance.js
+++ internship/inheritance/inheritance.js
@@ -1,35 +1,54 @@
+"use strict";
+
 class Base {
-  constructor() {
-    this.value;
+  constructor(value) {
+    this.value = value;
   }
 
   plus(...v) {
-    this.value = this.val + v.reduce((sum, current) => sum + current);
+    this.value = v.reduce((sum, current) => sum + current, this.value);
     return this;
   }
+
   minus(...v) {
-    const sum = v.reduce((sum, current) => sum + current);
-    this.value = this.value - sum;
+    if (typeof this.value === "number") {
+      const sum = v.reduce((sum, current) => sum + current);
+      this.value = this.value - sum;
+    } else {
+      this.value = this.value.slice(0, -v);
+    }
     return this;
   }
+
   multiply(v) {
-    this.value = this.value * v;
+    if (typeof this.value === "number") {
+      this.value = this.value * v;
+      return this;
+    } else {
+      this.value = this.value.repeat(v);
+    }
     return this;
   }
+
   divide(v) {
-    this.value = this.value / v;
-    return this;
+    if (typeof this.value === "number") {
+      this.value = this.value / v;
+      return this;
+    } else {
+      this.value = this.value.slice(0, v);
+      return this;
+    }
   }
+
   get() {
-    return this.value;
+    return this;
   }
 }
 
+// IntBuilder ---------------------------------------------
 class IntBuilder extends Base {
-  constructor(val) {
-    super(val);
-    val ? val : (val = 0);
-    this.val = val;
+  constructor(value = 0) {
+    super(value);
   }
   mod(n) {
     this.value = this.value % n;
@@ -44,21 +63,48 @@ class IntBuilder extends Base {
 
 const intBuilder = new IntBuilder(10);
 
-IntBuilder.random(10, 100);
+let ran = IntBuilder.random(10, 100);
 
-let num = intBuilder
-.plus(2, 3, 2)
-.minus(1, 2)
-.multiply(2)
-.divide(4)
-.mod(3)
-.get();
+let int = intBuilder
+  .plus(2, 3, 2) // 17;
+  .minus(1, 2) // 14;
+  .multiply(2) // 28;
+  .divide(4) // 7;
+  .mod(3) // 1;
+  .get(); // -> 1;
 
-console.log(num)
+// StrBuilder ---------------------------------------------
+function StringBuilder(value = "") {
+  Object.assign(this, new Base(value));
+}
 
-// console.log(intBuilder.plus(2, 3, 2).value);
-// console.log(intBuilder.minus(1, 2).value);
-// console.log(intBuilder.multiply(2).value);
-// console.log(intBuilder.divide(4).value);
-// console.log(intBuilder.mod(3).value);
-// console.log(intBuilder.get());
+StringBuilder.prototype = Object.create(Base.prototype);
+StringBuilder.prototype.constructor = StringBuilder;
+
+StringBuilder.prototype.remove = function (str) {
+  this.value = this.value
+    .split("")
+    .filter((s) => s !== str)
+    .join("");
+  return this;
+};
+
+StringBuilder.prototype.sub = function (from, n) {
+  this.value = this.value.split("").splice(from, n).join("");
+  return this;
+};
+
+let strBuilder = new StringBuilder("Hello");
+let s = strBuilder
+  .plus(" all", "!") // 'Hello all!'
+  .minus(4) // 'Hello '
+  .multiply(3) // 'Hello Hello Hello '
+  .divide(4) // 'Hell';
+  .remove("l") // 'He';
+  .sub(1, 1) // 'e';
+  .get(); // -> 'e';
+
+console.log(`
+Integer: ${int.value}, 
+random: ${ran}, 
+String: ${s.value}`);
