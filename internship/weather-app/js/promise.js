0c80bc862c0e0743ecf8160c2b92ec98e984141a
Emuallim <muhiddin001@gmail.com>
2021-09-03 23:19:33 +0500

    feat: add three different error handlers

--- internship/weather-app/js/promise.js
+++ internship/weather-app/js/promise.js
@@ -21,6 +21,7 @@ function weather() {
       }
     }).then(tashkent()).then(race())
     .catch((error) => {console.log(error)});
+    console.log(result);
 }
 
 function tashkent() {
@@ -47,5 +48,57 @@ function race(){
       result[2].push({ city: data.name, weather: temperature });
     })
     .catch((error) => {console.log(error)});
-    console.log(result);
 }
+
+function retry(operation, delay) {
+  return operation(). // run the operation
+      catch(function(reason) { // if it fails
+          return Q.delay(delay). // delay 
+             // retry with more time
+             then(retry.bind(null, operation, delay * 2)); 
+      });
+}
+
+// const retry = (fn, ms) => new Promise(resolve => { 
+//   fn()
+//     .then(resolve)
+//     .catch(() => {
+//       setTimeout(() => {
+//         console.log('retrying...');
+//         retry(fn, ms).then(resolve);
+//       }, ms);
+//     })
+// });
+
+// function errorHandling(){
+//   var t = 500;
+//   var max = 5;
+
+//   function rejectDelay(reason) {
+//     return new Promise(function(resolve, reject) {
+//       setTimeout(reject.bind(null, reason), t); 
+//     });
+//   }
+
+//   var p = Promise.reject();
+//   for(var i=0; i<max; i++) {
+//     p = p.catch(attempt).catch(rejectDelay);
+//   }
+//   p = p.then(processResult).catch(errorHandler);
+
+
+//   function attempt() {
+//     var rand = Math.random();
+//     if(rand < 0.8) {
+//       throw rand;
+//     } else {
+//       return rand;
+//     }
+//   }
+//   function processResult(res) {
+//     console.log(res);
+//   }
+//   function errorHandler(err) {
+//     console.error(err);
+//   }
+// }
