7d594d4cb8ee972319de272c133f5bb541b25197
Emuallim <muhiddin001@gmail.com>
2021-09-03 22:21:25 +0500

    fix: return fastest fetch first

--- internship/weather-app/js/promise.js
+++ internship/weather-app/js/promise.js
@@ -1,8 +1,9 @@
-let cities = ["New York","Paris","Sidney","Tashkent","Antarctica",["Minsk","Moscow","Kiev"]];
+let cities = ["New York","Paris","Sidney","Tashkent","Antarctica"];
+let racers = ["Minsk","Moscow","Kyiv"]
 const api = "a6d8992fdb1cbddae48cdee434095312";
 let result = [[], [], []];
 
-function threeCities() {
+function weather() {
   Promise.all([
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cities[0]}&appid=${api}&units=metric`),
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cities[1]}&appid=${api}&units=metric`),
@@ -18,9 +19,8 @@ function threeCities() {
         let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
         result[0].push({ city: data[i].name, weather: temperature });
       }
-    }).then(tashkent())
+    }).then(tashkent()).then(race())
     .catch((error) => {console.log(error)});
-    console.log(result);
 }
 
 function tashkent() {
@@ -32,9 +32,20 @@ function tashkent() {
       result[1].push({ city: data.name, weather: temperature });
     })
     .catch((error) => {console.log(error)});
-    console.log(result);
 }
 
-function racers(){
-  
+function race(){
+    Promise.race([
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${racers[0]}&appid=${api}&units=metric`),
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${racers[1]}&appid=${api}&units=metric`),
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${racers[2]}&appid=${api}&units=metric`),
+    ])
+    .then((response) => response.json())
+    .then((data) => {
+      let tempStr = Math.round(data.main.temp).toString();
+      let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
+      result[2].push({ city: data.name, weather: temperature });
+    })
+    .catch((error) => {console.log(error)});
+    console.log(result);
 }
