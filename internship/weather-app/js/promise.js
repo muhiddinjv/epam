9936cfddeeba56245b33bb047ce2a922e7a9c3c2
Emuallim <muhiddin001@gmail.com>
2021-09-03 10:18:39 +0500

    fix: get 3 constant cities

---
+++ internship/weather-app/js/promise.js
@@ -0,0 +1,52 @@
+let cities = [
+  "New York",
+  "Paris",
+  "Sidney",
+  "Tashkent",
+  "Antarctica",
+  ["Minsk","Moscow","Kiev"],
+];
+const api = "a6d8992fdb1cbddae48cdee434095312";
+let result = [[], [], []];
+
+function constant() {
+  Promise.all([
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cities[0]}&appid=${api}&units=metric`
+    ),
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cities[1]}&appid=${api}&units=metric`
+    ),
+    fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${cities[2]}&appid=${api}&units=metric`
+    ),
+  ])
+    .then((responses) => {
+      // Get a JSON object from each of the responses
+      return Promise.all(responses.map((response) => response.json()));
+    })
+    .then((data) => {
+      for (let i = 0; i < data.length; i++) {
+        let tempStr = Math.round(data[i].main.temp).toString();
+        let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
+        result[0].push({ city: data[i].name, weather: temperature });
+      }
+    })
+    .catch((error) => {console.log(error)});
+}
+
+function tashkent() {
+  constant();
+  fetch(
+    `http://api.openweathermap.org/data/2.5/weather?q=${cities[3]}&appid=${api}&units=metric`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      let tempStr = Math.round(data.main.temp).toString();
+      let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
+      result[1].push({ city: data.name, weather: temperature });
+
+      console.log(result);
+    })
+    .catch((error) => {console.log(error)});
+}
--- internship/weather-app/js/xmlhttp.js
+++ internship/weather-app/js/xmlhttp.js
@@ -1,46 +1,19 @@
-// function weather(city) {
-//   const api = "a6d8992fdb1cbddae48cdee434095312";
-
-//   var xhttp = new XMLHttpRequest();
-
-//   xhttp.onreadystatechange = function () {
-//     if (this.readyState == 4 && this.status == 200) {
-//       let json = JSON.parse(xhttp.responseText);
-//       displayResult(json);
-//     }
-//   };
-//   xhttp.open(
-//     "GET",
-//     `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`,
-//     true
-//   );
-//   xhttp.send();
-// }
-
-// const displayResult = (json) => {
-//   console.log(json);
-//   document.querySelector(".city1").innerHTML = `
-//     <div>${json.name} ${json.main.temp}</div>
-//   `;
-// };
-
 function weather() {
   const api = "a6d8992fdb1cbddae48cdee434095312";
-  let city = [
+  let cities = [
     "New York",
     "Paris",
     "Sidney",
     "Tashkent",
-    // ["Minsk", "Moscow", "Kyiv"],
   ];
-  let arr = [];
+  // let arr = [];
   let request = new XMLHttpRequest();
   (function loop(i, length) {
     if (i >= length) {
       return;
     }
 
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city[i]}&appid=${api}`;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=${api}`;
 
     request.open("GET", url);
     request.onreadystatechange = function () {
@@ -49,15 +22,16 @@ function weather() {
         request.status === 200
       ) {
         let data = JSON.parse(request.responseText);
+
         console.log(i + " " + data.name + " " + data.main.temp);
-        arr.push("city: "+ data.name + " " + data.main.temp);
+
         document.querySelector(".city1").innerHTML = `
-        <div>${data.name} ${data.main.temp}</div>
-      `;
+          <div>${data.name} ${data.main.temp}</div>
+        `;
         loop(i + 1, length);
       }
     };
     request.send();
-  })(0, city.length);
+  })(0, cities.length);
   console.log(arr)
 }
