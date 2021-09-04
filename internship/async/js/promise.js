af3ed5bb7096b50ee2c7ef8601452f17a7568542
Emuallim <muhiddin001@gmail.com>
2021-09-04 16:17:20 +0500

    feat: add errorhandling

--- internship/async/js/promise.js
+++ internship/async/js/promise.js
@@ -1,9 +1,16 @@
 let cities = ["New York","Paris","Sidney","Tashkent","Antarctica"];
 let racers = ["Minsk","Moscow","Kyiv"]
 const api = "a6d8992fdb1cbddae48cdee434095312";
-let result = [[], [], []];
+// const api = "d0889ce843a11270e6749177a5118aec"; old but working
 
-function weather() {
+let result = [[], [], []];
+let retryEveryMs = 1000;
+let maxRetries = 5;
+let retries = 0;
+
+
+const retry = () => new Promise((resolve,reject) => {
+  const showError = document.querySelector('.showError');
   Promise.all([
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cities[0]}&appid=${api}&units=metric`),
     fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cities[1]}&appid=${api}&units=metric`),
@@ -11,7 +18,7 @@ function weather() {
   ])
     .then((responses) => {
       // Get a JSON object from each of the responses
-      return Promise.all(responses.map((response) => response.json()));
+      return Promise.all(responses.map(res => res.json()));
     })
     .then((data) => {
       for (let i = 0; i < data.length; i++) {
@@ -20,19 +27,30 @@ function weather() {
         result[0].push({ city: data[i].name, weather: temperature });
       }
     }).then(tashkent()).then(race())
-    .catch((error) => {console.log(error)});
-    console.log(result);
-}
+    .catch((error) => {
+      console.log(error);
+      setTimeout(() => {
+        retries++;
+          showError.innerText = "Retrying failed promise..." + retries;
+          if(retries==maxRetries) {
+              return reject(showError.innerText = "Maximum retries exceeded!");
+          }
+          retry().then(resolve);
+      }, retryEveryMs);
+      console.log(retries)
+  });
+  console.log(result);
+})
 
 function tashkent() {
-  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cities[3]}&appid=${api}&units=metric`)
-    .then((response) => response.json())
-    .then((data) => {
+  fetch(`http://api.openwathermap.org/data/2.5/weather?q=${cities[3]}&appid=${api}&units=metric`)
+  .then(res => res.json())
+    .then(data => {
       let tempStr = Math.round(data.main.temp).toString();
       let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
       result[1].push({ city: data.name, weather: temperature });
     })
-    .catch((error) => {console.log(error)});
+    .catch(error => console.log(error));
 }
 
 function race(){
@@ -41,34 +59,45 @@ function race(){
       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${racers[1]}&appid=${api}&units=metric`),
       fetch(`http://api.openweathermap.org/data/2.5/weather?q=${racers[2]}&appid=${api}&units=metric`),
     ])
-    .then((response) => response.json())
-    .then((data) => {
+    .then(res => res.json())
+    .then(data => {
       let tempStr = Math.round(data.main.temp).toString();
       let temperature = tempStr > 0 ? "+" + tempStr : tempStr;
       result[2].push({ city: data.name, weather: temperature });
     })
-    .catch((error) => {console.log(error)});
+    .catch(error => console.log(error));
 }
 
-function retry(operation, delay) {
-  return operation(). // run the operation
-      catch(function(reason) { // if it fails
-          return Q.delay(delay). // delay 
-             // retry with more time
-             then(retry.bind(null, operation, delay * 2)); 
-      });
-}
+// function fetchWithAutoRetry(weather, maxRetryCount){
+//   return new Promise((resolve, reject)=>{
+//     let retries = 0;
+//     const caller = () =>{
+//       weather()
+//       .then(data=>resolve(data))
+//       .catch(error=>{
+//         if(retries < maxRetryCount){
+//           retries++;
+//           caller();
+//         }else{reject(error)}
+//       })
+//     }
+//     retries=1;
+//     caller();
+//   })
+// }
 
-// const retry = (fn, ms) => new Promise(resolve => { 
-//   fn()
-//     .then(resolve)
-//     .catch(() => {
-//       setTimeout(() => {
-//         console.log('retrying...');
-//         retry(fn, ms).then(resolve);
-//       }, ms);
-//     })
-// });
+// fetchWithAutoRetry(weather, 5);
+
+// function retry(weather, delay) {
+//   return weather(). // run the operation
+//       catch(function(reason) { // if it fails
+//           return Q.delay(delay). // delay 
+//              // retry with more time
+//              then(retry.bind(null, operation, delay * 2)); 
+//       });
+// }
+
+  
 
 // function errorHandling(){
 //   var t = 500;
